ef8a8b3d05c1d1bce20421b47c5ee3675bbb956c
ransurf <ransomguest@gmail.com>
2022-01-15 18:53:40 -0800

    create sign up page, start video

--- only-profs/src/pages/Profile.js
+++ only-profs/src/pages/Profile.js
@@ -5,10 +5,10 @@ import './Profile.css';
 function Profile() {
     return (
         <div className="profile-container">
-            <div className="prof-info">
+            <div className="profile-info">
                 <h1>Name</h1>
                 <h2>Title</h2>
-                <p>Bio</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div>
                 <h1>Lectures</h1>
